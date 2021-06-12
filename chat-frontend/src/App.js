5ff62be519753336a7743f9e6d5eb39ff20d6819
Evgeni67 <evgeni313@abv.bg>
2021-06-13 03:09:25 +0500

    update

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 import Login from "./components/login/login";
 import Chat from "./components/chat/chat";
 import React, { Component } from "react";
-import { BrowserRouter as Router,  Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 class App extends Component {
+  componentDidMount = () => 
+  {
+    if(window.location.href === "https://chatio-frontend.herokuapp.com/"){
+      window.location.href = "https://chatio-frontend.herokuapp.com/login"
+    }
+  }
   render() {
     return (
       <div className="ChatApp">
